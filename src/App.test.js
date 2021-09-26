27e3a0f2da84b4214a7d3a6999b356749329afe1
christianhart <christianphart0@gmail.com>
2021-09-26 00:32:25 -0400

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Benefits', () => {
+test('renders Yearly', () => {
   render(<App />);
   const linkElement = screen.getByText(/Yearly/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Your Benefits', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Your Benefits/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Monthly', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Monthly/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders One-Off', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/One-Off/i);
   expect(linkElement).toBeInTheDocument();
 });
